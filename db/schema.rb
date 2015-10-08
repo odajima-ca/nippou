e730c7b6633dceb151c3af4fc99ec9b1473f82ad
Yu Odajima <odajima_yu_gn@cyberagent.co.jp>
2015-10-08 18:33:46 +0900

    カテゴリマスタを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151008092735) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
