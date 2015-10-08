1d86dd780d2449d427bb6dc4b962a1925cb44fd8
Yu Odajima <odajima_yu_gn@cyberagent.co.jp>
2015-10-08 20:18:28 +0900

    トップページをタスク一覧に変更する

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe TasksController, type: :routing do
   describe 'routing' do
 
     it 'routes to #index' do
+      expect(get: '/').to route_to('tasks#index')
       expect(get: '/tasks').to route_to('tasks#index')
     end
 
