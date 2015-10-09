1803af7c553cf42cc7e8dccf48bf63632423b201
Yu Odajima <odajima_yu_gn@cyberagent.co.jp>
2015-10-09 14:24:48 +0900

    レポート内容を実装する

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -1,4 +1,8 @@
 class DailyReportsController < ApplicationController
   def index
+    @today = Date.today
+    @todo_tasks = Task.todo
+    @doing_tasks = Task.doing
+    @done_tasks = Task.done.where(completed_at: @today.beginning_of_day..@today.end_of_day)
   end
 end
--- spec/controllers/daily_reports_controller_spec.rb
+++ spec/controllers/daily_reports_controller_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe DailyReportsController, type: :controller do
 
   describe 'GET #index' do
+    let!(:todo_tasks) { FactoryGirl.create_list(:task, 1, status: :todo) }
+    let!(:doing_tasks) { FactoryGirl.create_list(:task, 2, status: :doing) }
+    let!(:done_tasks) { FactoryGirl.create_list(:task, 3, status: :done, completed_at: Time.current) }
+    let!(:old_done_tasks) { FactoryGirl.create_list(:task, 1, status: :done, completed_at: 1.days.ago) }
+
     it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
+      expect(assigns(:today)).to eq Date.today
+      expect(assigns(:todo_tasks)).to match_array todo_tasks
+      expect(assigns(:doing_tasks)).to match_array doing_tasks
+      expect(assigns(:done_tasks)).to match_array done_tasks
     end
   end
-
 end
